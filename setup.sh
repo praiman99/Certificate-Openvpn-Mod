115b8be733a0bda9d87f6c406605f014c0932abf
PR Aiman <58582776+praiman99@users.noreply.github.com>
2023-07-11 00:41:08 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,6 +19,72 @@ cp /etc/openvpn/server/server.crt /etc/openvpn/server.crt
 cp /etc/openvpn/server/server.key /etc/openvpn/server.key
 chmod +x /etc/openvpn/ca.crt
 
+cd
+mkdir -p /usr/lib/openvpn/
+cp /usr/lib/x86_64-linux-gnu/openvpn/plugins/openvpn-plugin-auth-pam.so /usr/lib/openvpn/openvpn-plugin-auth-pam.so
+
+# nano /etc/default/openvpn
+sed -i 's/#AUTOSTART="all"/AUTOSTART="all"/g' /etc/default/openvpn
+
+# aktifkan ip4 forwarding
+echo 1 > /proc/sys/net/ipv4/ip_forward
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+
+# Remove default Create New
+cd
+rm /etc/openvpn/*.conf
+
+# Buat config server TCP 1194
+cd /etc/openvpn
+cat > /etc/openvpn/server-tcp-1194.conf <<-EOF
+port 1194
+proto tcp
+dev tun
+ca ca.crt
+cert server.crt
+key server.key
+dh dh2048.pem
+plugin /usr/lib/openvpn/openvpn-plugin-auth-pam.so login
+verify-client-cert none
+username-as-common-name
+server 10.6.0.0 255.255.255.0
+ifconfig-pool-persist ipp.txt
+push "redirect-gateway def1 bypass-dhcp"
+push "dhcp-option DNS 1.1.1.1"
+push "dhcp-option DNS 1.0.0.1"
+keepalive 5 30
+comp-lzo
+persist-key
+persist-tun
+status /var/log/openvpn/server-tcp-1194.log
+verb 3
+EOF
+
+# Buat config server UDP 2200
+cat > /etc/openvpn/server-udp-2200.conf <<-EOF3
+port 2200
+proto udp
+dev tun
+ca ca.crt
+cert server.crt
+key server.key
+dh dh2048.pem
+plugin /usr/lib/openvpn/openvpn-plugin-auth-pam.so login
+verify-client-cert none
+username-as-common-name
+server 10.7.0.0 255.255.255.0
+ifconfig-pool-persist ipp.txt
+push "redirect-gateway def1 bypass-dhcp"
+push "dhcp-option DNS 1.1.1.1"
+push "dhcp-option DNS 1.0.0.1"
+keepalive 5 30
+comp-lzo
+persist-key
+persist-tun
+status /var/log/openvpn/server-udp-2200.log
+verb 3
+EOF3
+
 # Delete script
 history -c
 sleep 1

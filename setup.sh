035920bd1a95a44f2784e882a03160a738b62feb
PR Aiman <58582776+praiman99@users.noreply.github.com>
2023-07-10 22:32:38 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@ rm -f /etc/openvpn/vpn.zip
 chown -R root:root /etc/openvpn/server
 # server config 
 cp /etc/openvpn/server/ca.crt /etc/openvpn/ca.crt
-cp /etc/openvpn/server/easy-rsa/server/dh2048.pem /etc/openvpn/dh2048.pem
-cp /etc/openvpn/server/easy-rsa/server/dh.pem /etc/openvpn/dh.pem
+cp /etc/openvpn/server/dh2048.pem /etc/openvpn/dh2048.pem
+cp /etc/openvpn/server/dh.pem /etc/openvpn/dh.pem
 cp /etc/openvpn/server/server.crt /etc/openvpn/server.crt
 cp /etc/openvpn/server/server.key /etc/openvpn/server.key
 chmod +x /etc/openvpn/ca.crt

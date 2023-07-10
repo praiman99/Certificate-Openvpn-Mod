3e20233e09450106a9c5ec85f2c3a14cf6eee237
PR Aiman <58582776+praiman99@users.noreply.github.com>
2023-07-10 22:17:49 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,10 +12,10 @@ unzip /etc/openvpn/vpn.zip
 rm -f /etc/openvpn/vpn.zip 
 chown -R root:root /etc/openvpn/server
 # server config 
-cp -All/etc/openvpn/server/ca.crt /etc/openvpn/ca.crt
-cp -All /etc/openvpn/server/easy-rsa/server/dh2048.pem /etc/openvpn/dh2048.pem
-cp -All /etc/openvpn/server/server.crt /etc/openvpn/server.crt
-cp -All /etc/openvpn/server/server.key /etc/openvpn/server.key
+cp -A /etc/openvpn/server/ca.crt /etc/openvpn/ca.crt
+cp -A /etc/openvpn/server/easy-rsa/server/dh2048.pem /etc/openvpn/dh2048.pem
+cp -A /etc/openvpn/server/server.crt /etc/openvpn/server.crt
+cp -A /etc/openvpn/server/server.key /etc/openvpn/server.key
 chmod +x /etc/openvpn/ca.crt
 
 # Delete script
